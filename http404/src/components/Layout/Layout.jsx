59f29e572cd9e9d0fd13c69b174572fcc591c08c
Witold Nawrot <witold.naw@gmail.com>
2023-01-22 15:12:09 +0100

    add navigation

--- http404/src/components/Layout/Layout.jsx
+++ http404/src/components/Layout/Layout.jsx
@@ -1,13 +1,11 @@
 import { Outlet} from "react-router"
 import Footer from "./Footer/Footer"
-import HeroContent from "./HeroContent/HeroContent"
 import Navigation  from "./Navigation/Navigation"
 
 const Layout = () => {
     return (
         <>
         <Navigation/>
-        <HeroContent/>
         <Footer />
         <Outlet/>
         </>
--- http404/src/components/Layout/Navigation/Navigation.jsx
+++ http404/src/components/Layout/Navigation/Navigation.jsx
@@ -5,28 +5,88 @@ import avatar from "./atena_avatar1.png";
 
 const Navigation = () => {
   return (
-    <div className={styles.navigation}>
+    <nav className={styles.navigation}>
       <img className={styles.logoAtena} src={logo} alt="logo Atena"></img>
-
       <div className={styles.navigationDetails}>
         <div className={styles.btnWrapper}>
-          <NavLink to="signin">
-            <button>Zaloguj się</button>
-          </NavLink>
-          <NavLink to="signup">
-            <button>Zarejestruj się</button>
-          </NavLink>
+              <NavLink to="auth/signin">
+                <button>Zaloguj się</button>
+              </NavLink>
+              <NavLink to="auth/signup">
+                <button>Zarejestruj się</button>
+              </NavLink>
+          
+              {/* <NavLink to="admin">
+                <button className={styles.btn}>Panel klienta</button>
+              </NavLink> */}
+
+          {/* <button className={styles.btn}>
+            Wyloguj się
+          </button> */}
         </div>
-        <div className={styles.avatarWrapper}>
-          <img
-            className={styles.avatarAtena}
-            src={avatar}
-            alt="avatar Atena"
-          ></img>
-        </div>
+      
+      <div className={styles.avatarWrapper}>
+        <img
+          className={styles.avatarAtena}
+          src={avatar}
+          alt="avatar Atena"
+        ></img>
       </div>
-    </div>
+      </div>
+    </nav>
   );
 };
 
 export default Navigation;
+
+
+
+
+
+// import { NavLink } from "react-router-dom";
+// import { signOut } from "firebase/auth";
+// import { auth } from "../Api/firebase";
+// import styles from "./Navigation.module.css";
+// import logo from "./atena_logo1.png";
+// import avatar from "./atena_avatar1.png";
+
+// const Navigation = ({isAuth, email}) => {
+//   return (
+//     <nav className={styles.navigation}>
+//       <img className={styles.logoAtena} src={logo} alt="logo Atena"></img>
+//       <div className={styles.navigationDetails}>
+//         <div className={styles.btnWrapper}>
+//           {!isAuth && (
+//             <>
+//               <NavLink to="auth/signin">
+//                 <button>Zaloguj się</button>
+//               </NavLink>
+//               <NavLink to="auth/signup">
+//                 <button>Zarejestruj się</button>
+//               </NavLink>
+//             </>
+//           )}
+//           {isAuth && (
+//             <>
+//               <NavLink to="admin">
+//                 <button className={styles.btn}>Panel klienta</button>
+//               </NavLink>
+//             </>
+//           )}
+//           <button className={styles.btn} onClick={() => signOut(auth)}>
+//             Wyloguj się
+//           </button>
+//         </div>
+//       </div>
+//       <div className={styles.avatarWrapper}>
+//         <img
+//           className={styles.avatarAtena}
+//           src={avatar}
+//           alt="avatar Atena"
+//         ></img>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navigation;
