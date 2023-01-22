8e0bf4038969d9c19aceccc60d419d2972556b81
Ewelina <ebialoszycka@gmail.com>
2023-01-22 19:49:10 +0100

    added pagination

--- http404/src/components/Layout/Layout.jsx
+++ http404/src/components/Layout/Layout.jsx
@@ -1,14 +1,14 @@
-import { Outlet} from "react-router"
-import Footer from "./Footer/Footer"
-import Navigation  from "./Navigation/Navigation"
+import { Outlet } from "react-router";
+import Footer from "./Footer/Footer";
+import Navigation from "./Navigation/Navigation";
 
 const Layout = () => {
-    return (
-        <>
-        <Navigation/>
-        <Footer />
-        <Outlet/>
-        </>
-    )
-    }
+  return (
+    <>
+      <Navigation />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 export default Layout;
