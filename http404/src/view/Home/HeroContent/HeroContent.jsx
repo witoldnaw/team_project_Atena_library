c70be2c43b851f091949a87854612172a1ccfc2e
Ewelina <ebialoszycka@gmail.com>
2023-01-29 20:16:18 +0100

    change english text

--- http404/src/view/Home/HeroContent/HeroContent.jsx
+++ http404/src/view/Home/HeroContent/HeroContent.jsx
@@ -1,26 +1,28 @@
-import { Link } from "react-router-dom"
-import styles from "./HeroContent.module.css"
-import illustration from "./illustration_hero_header1.png"
-import illustrationWave from "./illustration_hero_header_wave.png"
-
-
-
+import { Link } from "react-router-dom";
+import styles from "./HeroContent.module.css";
+import illustration from "./illustration_hero_header1.png";
+import illustrationWave from "./illustration_hero_header_wave.png";
 
 const HeroContent = () => {
-    return (
+  return (
+    <div className={styles.headerWrapper}>
+      <div className={styles.illustrationWrapper}>
+        <img
+          className={styles.illustration}
+          src={illustration}
+          alt="illustration"
+        ></img>
+      </div>
+      <div className={styles.description}>
+        <h1>
+          "Kto czyta książki <br></br> żyje podwójnie"
+        </h1>
+        <Link to="searching">
+          <button className={styles.btn}>Wyszukaj książkę</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-        <div className={styles.headerWrapper}>
-            <div className={styles.illustrationWrapper}>
-            <img className={styles.illustration} src={illustration} alt="illustration" ></img>
-        </div>
-        <div className={styles.description}>
-            <h1>"Kto czyta książki <br></br> żyje podwójnie"</h1>
-            <Link to="searching">
-            <button className={styles.btn}>wyszukaj ksiązkę</button>
-            </Link>
-        </div>
-        </div>
-    )
-}
-
-export default HeroContent
+export default HeroContent;
