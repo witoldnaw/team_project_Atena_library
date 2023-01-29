a1257ef628e7c0896ef40965b0cbad3f00ef4a22
Ewelina <ebialoszycka@gmail.com>
2023-01-29 21:07:45 +0100

    styling register forms

--- http404/src/view/Auth/Form/FormLogin.jsx
+++ http404/src/view/Auth/Form/FormLogin.jsx
@@ -1,26 +1,27 @@
-import styles from "./Form.module.css"
-import picture from "./illustration_atena_login.png"
+import styles from "./Form.module.css";
+import picture from "./illustration_atena_login.png";
 
-export const FormLogin = ({ submitText, isPasswordHidden = false, onSubmit }) => (
-  
-
+export const FormLogin = ({
+  submitText,
+  isPasswordHidden = false,
+  onSubmit,
+}) => (
   <div className={styles.formWrapper}>
     <img className={styles.picture} src={picture} alt="picture books"></img>
-    <form onSubmit={onSubmit} >
+    <form onSubmit={onSubmit}>
       <div className={styles.labelWrapper}>
-        <label htmlFor="email">E-mail</label>
+        <label htmlFor="email">E-mail:</label>
         <input type="email" name="email" id="email" />
       </div>
       {!isPasswordHidden && (
         <div className={styles.labelWrapper}>
-          <label htmlFor="password">Hasło</label>
+          <label htmlFor="password">Hasło:</label>
           <input type="password" name="password" id="password" />
         </div>
       )}
       <div className={styles.btnWrapper}>
-      <button className={styles.btnForm}>{submitText}</button>
+        <button className={styles.btnForm}>{submitText}</button>
       </div>
     </form>
-    </div>
-    
-  )
+  </div>
+);
