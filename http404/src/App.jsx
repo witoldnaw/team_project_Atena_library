abcb10046e0729ee850d22e78c3fc8692539cfaa
Witold Nawrot <witold.naw@gmail.com>
2023-01-29 18:45:12 +0100

    organization of folders

--- http404/src/App.jsx
+++ http404/src/App.jsx
@@ -1,16 +1,16 @@
 import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
 import { useContext } from "react";
 import "./App.css";
-import { Profile } from "./components/Profile/Profile";
-import { Register } from "./components/Register/Register";
-import { Login } from "./components/Login/Login";
-import { ForgotPassword } from "./components/ForgotPassword/ForgotPassword";
+import { Register } from "./view/Auth/Register/Register";
+import { Login } from "./view/Auth/Login/Login";
+import { ForgotPassword } from "./view/Auth/ForgotPassword/ForgotPassword";
 import Home from "./view/Home/Home";
 import Navigation from "./components/Layout/Navigation/Navigation";
 import { BookDetails } from "./view/BookDetails/BookDetails";
 import { userDataContext } from "./components/UserDataContext/UserDataContext";
 import SearchingSite from "./view/SearchingSite/SearchingSite";
 import Footer from "./components/Layout/Footer/Footer";
+import { Profile } from "./view/Auth/Profile/Profile";
 
 function App() {
   const { user, role } = useContext(userDataContext);
