4530bc177514a8b98438a9e86b93aa708882f703
Ewelina <ebialoszycka@gmail.com>
2023-01-30 21:52:18 +0100

    change search resoults

--- http404/src/view/SearchingSite/SearchList/SearchList.jsx
+++ http404/src/view/SearchingSite/SearchList/SearchList.jsx
@@ -7,39 +7,79 @@ import BookCard from "../../Home/ListBooks/BookCard/BookCard";
 
 const SearchList = () => {
   const [books, setBooks] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [lastSearchQuery, setLastSearchQuery] = useState("");
+  const [searchState, setSearchState] = useState(false);
 
-  const getBooks = () => {
-    if (searchQuery.length === 0) {
+  const getBooks = (query) => {
+    if (query.length === 0) {
+      console.log("Puste query");
+      setLastSearchQuery(query);
+      setSearchState(true);
       setBooks([]);
       return;
     }
+
     const booksCollection = collection(db, "books");
     getDocs(booksCollection).then((querySnapshot) => {
       const allBooks = querySnapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
       }));
-      console.log("wszytskie książki:", allBooks);
+      console.log("wszystkie książki:", allBooks);
       const filteredBooks = allBooks.filter(
         (book) =>
-          book.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
-          book.author.toLowerCase().includes(searchQuery.toLowerCase())
+          book.title.toLowerCase().includes(query.toLowerCase()) ||
+          book.author.toLowerCase().includes(query.toLowerCase())
       );
 
       console.log("przefiltrowane książki:", filteredBooks);
+      setLastSearchQuery(query);
       setBooks(filteredBooks);
+      setSearchState(true);
     });
   };
-
-  const search = () => {
-    console.log("search: [", searchQuery, "]");
-    getBooks();
+  const performSearch = () => {
+    getBooks(inputValue);
   };
-  const updateSearchQuery = (event) => {
-    setSearchQuery(event.target.value);
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      performSearch();
+    }
+  };
+  const updateInputValue = (event) => {
+    // console.log("updateInputValue", event.target.value);
+    setInputValue(event.target.value);
   };
 
+  const queryResult = () => {
+    if (searchState === false) {
+      return (
+        <p className={styles.listBooksFail}>
+          Na pewno tutaj znajdziesz coś dla siebie! Wyszukaj książkę po tytule
+          lub autorze.
+        </p>
+      );
+    }
+    if (lastSearchQuery.length === 0 && books.length === 0) {
+      return <p className={styles.listBooksFail}>Spróbuj ponownie.</p>;
+    }
+    if (lastSearchQuery.length > 0 && books.length === 0) {
+      return (
+        <p className={styles.listBooksFail}>
+          Nie mamy książki, której szukasz :(. <br />
+          Spróbuj ponownie - może znajdziesz coś innego :)
+        </p>
+      );
+    }
+    return (
+      <div className={styles.cards}>
+        {books.map((book) => {
+          return <BookCard key={book.id} book={book} />;
+        })}{" "}
+      </div>
+    );
+  };
   return (
     <div className={styles.SearchWrapper}>
       <div className={styles.SearchBanner}>
@@ -51,29 +91,19 @@ const SearchList = () => {
         <div className={styles.SearchBox}>
           <p className={styles.SearchDescription}>Co chcesz przeczytać?</p>
           <input
+            onKeyDown={handleKeyDown}
             className={styles.SearchInput}
-            onChange={updateSearchQuery}
+            onChange={updateInputValue}
             placeholder="Wyszukaj książkę"
           />
-          <button className={styles.SearchButton} onClick={search}>
+          <button className={styles.SearchButton} onClick={performSearch}>
             Szukaj
           </button>
         </div>
       </div>
       <div className={styles.listBookWrapper}>
         <span className={styles.ListBooks}>Wyniki wyszukiwania:</span>
-        {books.length === 0 ? (
-          <p className={styles.listBooksFail}>
-            Nie mamy książki, której szukasz :(. <br />
-            Spróbuj ponownie - może znajdziesz coś innego :){" "}
-          </p>
-        ) : (
-          <div className={styles.cards}>
-            {books.map((book) => {
-              return <BookCard key={book.id} book={book} />;
-            })}
-          </div>
-        )}
+        {queryResult()}
       </div>
     </div>
   );
