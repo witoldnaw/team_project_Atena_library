492a16ff7ee3e1246f5ce5530f8236f8842caa6d
Witold Nawrot <witold.naw@gmail.com>
2023-01-31 01:02:15 +0100

    panel admin - add input and label

--- http404/src/view/Admin/Admin.jsx
+++ http404/src/view/Admin/Admin.jsx
@@ -5,6 +5,42 @@ import BookCard from "../../view/Home/ListBooks/BookCard/BookCard";
 import styles from "../../view/Home/ListBooks/ListBooks.module.css";
 import ReactPaginate from "react-paginate";
 
+
+
+export const Admin = () => {
+
+    return (
+<>
+<h2>Formularz dodawania ksiązek</h2>
+
+<form class="add">
+    <label for="title">Title:</label>
+    <input type="text" name="title" required></input>
+    <label for="author">Author:</label>
+    <input type="text" name="author" required></input>
+    <label for="description">Opis</label>
+    <input type="text" name="description" required></input>
+    <label for="status">Dostępna</label>
+    <input type="radio" name="status" value="dostępna" required></input>
+    <label for="status">Nie dostępna</label>
+    <input type="radio" name="status" value="nie dostępna" required></input>
+    <input type="hidden" name="userId" value=""></input>
+    <label for="imageURL">Link do zdjęcia okładki</label>
+    <input type="url" name="imageURL" required></input>
+<br></br>
+    <button>dodaj ksiazke</button>
+</form>
+
+<form class="delete">
+
+<label for="id">Document id</label>
+<input type="text" name="id" required></input>
+<button>usun ksiazke</button>
+</form>
+</>
+    )
+}
+
 const colRef = collection(db, "books")
 
 const addBookForm = document.querySelector(".add")
@@ -23,39 +59,3 @@ addBookForm.addEventListener('submit', (e) => {
         addBookForm.reset()
     })
 })
-
-export const Admin = () => {
-
-    return (
-<>
-<h2>Formularz dodawania ksiązek</h2>
-
-<form class="add">
-    <label for="title">Title:</label>
-    <input type="text" name="title" required></input>
-    <label for="author">Author:</label>
-    <input type="text" name="author" required></input>
-    <label for="description">Opis</label>
-    <input type="text" name="description" required></input>
-    {/* <label for="status">Dostępna</label>
-    <input type="radio" name="status" required></input>
-    <label for="status">Nie dostępna</label>
-    <input type="radio" name="status" required></input> */}
-    <input type="hidden" name="userId" value=""></input>
-    <label for="imageURL">Link do zdjęcia okładki</label>
-    <input type="url" name="imageURL" required></input>
-<br></br>
-<input type="submit" value="submit"></input>
-    <button>dodaj ksiazke</button>
-</form>
-
-<form class="delete">
-
-<label for="id">Document id</label>
-<input type="text" name="id" required></input>
-<button>usun ksiazke</button>
-</form>
-</>
-    )
-}
-
