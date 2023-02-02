fb5d68d98a772ab883aecd5a19b54080fda664bd
Jakub Bedynek <kuba.xray@gmail.com>
2023-02-02 19:54:30 +0100

    added functionality of deleting books from user Profile

--- http404/src/view/Auth/Profile/Profile.jsx
+++ http404/src/view/Auth/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import styles from "./Profile.module.css";
 import avatar from "../../../assets/atena_avatar1.png";
 import { userDataContext } from "../../../components/UserDataContext/UserDataContext";
-import { UserBooksList } from "../../../components/UserBooksList/UserBooksList";
+import { UserBooksList } from "../../../components/UserBooks/UserBooksList/UserBooksList";
 
 export const Profile = () => {
   const { userData } = useContext(userDataContext);
@@ -30,27 +30,6 @@ export const Profile = () => {
       <section className={styles.user_books}>
         <h2 className={styles.user_shelf}>Twoja Półka:</h2>
         <ul className={styles.books}>
-          {/* <li>
-            <img src="" alt="book cover" />
-          </li>
-          <li>
-            <ul className={styles.book_details}>
-              <li>
-                <p>Tytuł:</p>
-              </li>
-              <li>
-                <p>Autor:</p>
-              </li>
-              <li>
-                Opis: Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Consequuntur libero odit impedit, esse porro aperiam quae
-                voluptatum, eaque labore ducimus nobis architecto magni. Rem,
-                harum! Animi nobis dolore distinctio deserunt!
-              </li>
-              <li>Data wypożyczenia 20.01.2023</li>
-              <li>Data Zwrotu 20.02.2023</li>
-            </ul>
-          </li> */}
           <UserBooksList />
         </ul>
         <div className={styles.button_wrapper}>
