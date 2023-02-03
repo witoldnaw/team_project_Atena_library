69be52cd9904735cec20e43bc07198ff70a55787
Ewelina <ebialoszycka@gmail.com>
2023-02-03 09:25:56 +0100

    wip

--- http404/src/components/Filtration/Filter.jsx
+++ http404/src/components/Filtration/Filter.jsx
@@ -2,8 +2,14 @@ import styles from "./Filtration.module.css";
 
 const Filter = ({ genre, onGenreChange }) => {
   return (
+
     <div className={styles.filter}>
-      <button onClick={() => onGenreChange(genre)}>{genre.name}</button>
+      <label for={genre.name} className={styles.labelFilter}>
+      <input type="radio" id={genre.name} value={genre.name} name="cetegory" onClick={() => onGenreChange(genre)}/>
+      <span>{genre.name}</span>
+      </label>
+      {/* <button onClick={() => onGenreChange(genre)}>{genre.name}</button> */}
+
     </div>
   );
 };
