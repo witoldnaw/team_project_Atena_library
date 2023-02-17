2c4b1ce37dd2c3f7865a4e7191e55440f8101ac1
Ewelina <ebialoszycka@gmail.com>
2023-02-17 14:18:08 +0100

    added authors

--- http404/src/components/Layout/Footer/Footer.jsx
+++ http404/src/components/Layout/Footer/Footer.jsx
@@ -3,13 +3,47 @@ import illustration_books_footer from "./illustration_books_footer.png";
 
 const Footer = () => {
   return (
-    <div className={styles.footer}>
-      <img
-        className={styles.img}
-        src={illustration_books_footer}
-        alt="books"
-      ></img>
-      <p className={styles.pFooter}>Biblioteka Atena</p>
+    <div className={styles.footerWrapper}>
+      <div className={styles.footerLogo}>
+        <img
+          className={styles.imgLogo}
+          src={illustration_books_footer}
+          alt="books"
+        ></img>
+        <p className={styles.pFooter}>Biblioteka Atena</p>
+      </div>
+      <div className={styles.footerAuthors}>
+        <ul className={styles.footerAuthorsList}>
+          Autorzy:
+          <li className={styles.footerAuthor}>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/EwelinaBialoszycka"
+            >
+              Ewelina Białoszycka
+            </a>
+          </li>
+          <li className={styles.footerAuthor}>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/witoldnaw"
+            >
+              Witold Nawrot
+            </a>
+          </li>
+          <li className={styles.footerAuthor}>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/JackRaven611"
+            >
+              Jakub Bedynek{" "}
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
